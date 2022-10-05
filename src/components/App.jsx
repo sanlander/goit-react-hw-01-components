b34669968c3ce95b95364614ff3c181dfa073899
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-10-05 19:06:01 +0300

    hw-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Profile } from 'components/Profile';
+import user from '../user.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        // height: '100vh',
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // fontSize: 40,
+        // color: '#010101',
+        textAlign: 'center',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
   );
 };
